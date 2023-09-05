920f555908045878bd253b544e9e7fd309f89847
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-06 00:23:51 +0300

    feat: complete exercise 3.4

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.get('/info', (req, res) => {
 	<p>${new Date()}</p>`)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(person => person.id !== id)
+
+	res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
