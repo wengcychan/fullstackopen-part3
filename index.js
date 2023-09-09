589ae3351acc1cc460cc73c3b80d0d337c05b13f
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-09 15:39:08 +0300

    feat: complete exercise 3.17

--- index.js
+++ index.js
@@ -75,6 +75,21 @@ app.post('/api/persons', (req, res) => {
 	})
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body
+
+	const person = {
+		name: body.name,
+		number: body.number
+	}
+
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+	.then(updatedPerson => {
+		res.json(updatedPerson)
+	})
+	.catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
 	Person.findByIdAndRemove(req.params.id).then(result => {
 		res.status(204).end()
